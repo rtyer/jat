5d65d34e4faf944f391cd68ddbeaffb6d9ad1e92
localadmin <localadmin@keel.f4tech.com>
2008-04-29 14:49:54 -0600

    Removed old ruby stuff.

--- lib/core.rb
+++
@@ -1,6 +0,0 @@
-class String
-  def to_fully_qualified_java_classname(class_dir)
-    classname = self.sub(Regexp.new(class_dir), '')
-    classname.sub(/.class$/, '').sub(/^\//, '').tr('/', '.')
-  end
-end
