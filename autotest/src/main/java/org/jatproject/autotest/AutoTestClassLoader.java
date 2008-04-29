aed6d87aad4bdf5ba7c25771da3651dcc67dfaf3
localadmin <localadmin@keel.f4tech.com>
2008-04-29 14:44:47 -0600

    Implemented the guts of AutoTestClassLoader.
    Should be able to reload a class now.

--- autotest/src/main/java/org/jatproject/autotest/AutoTestClassLoader.java
+++ autotest/src/main/java/org/jatproject/autotest/AutoTestClassLoader.java
@@ -1,12 +1,36 @@
 package org.jatproject.autotest;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 public class AutoTestClassLoader extends ClassLoader
 {
     public Class defineClass(String className, File file) throws ClassNotFoundException
     {
-        // defineClass(name, classBytes, 0, classBytes.length)       
-        return null;
+        try
+        {
+            FileChannel channel = new FileInputStream(file).getChannel();
+
+            try
+            {
+                int size = (int) channel.size();
+
+                ByteBuffer buffer = ByteBuffer.allocate(size);
+                channel.read(buffer);
+
+                return defineClass(className, buffer.array(), 0, size);
+            }
+            finally
+            {
+                channel.close();
+            }
+        }
+        catch (IOException e)
+        {
+            throw new ClassNotFoundException(className);
+        }
     }
 }
