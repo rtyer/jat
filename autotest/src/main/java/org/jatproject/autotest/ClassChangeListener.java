6a3da600194437075f9ed1cee144a895d6508987
Adam Esterline <aesterline@aesterline.f4tech.com>
2008-04-30 12:49:19 -0600

    Removed unused classes.

--- autotest/src/main/java/org/jatproject/autotest/ClassChangeListener.java
+++
@@ -1,6 +0,0 @@
-package org.jatproject.autotest;
-
-public interface ClassChangeListener
-{
-    void changedClasses(Class... changed);
-}
--- autotest/src/main/java/org/jatproject/autotest/ClassMonitor.java
+++
@@ -1,6 +0,0 @@
-package org.jatproject.autotest;
-
-public interface ClassMonitor
-{
-    void addListener(ClassChangeListener listener);
-}
