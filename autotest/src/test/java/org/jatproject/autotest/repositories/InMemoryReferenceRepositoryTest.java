0884644fe7b7573467657604b8e3bc45480ac2c0
James Estes <james@Macintosh.local>
2008-05-05 08:03:43 -0600

    forgot to add the files for the InMemory repo

---
+++ autotest/src/test/java/org/jatproject/autotest/repositories/InMemoryReferenceRepositoryTest.java
@@ -0,0 +1,31 @@
+package org.jatproject.autotest.repositories;
+
+import org.jmock.Mockery;
+import org.jmock.lib.legacy.ClassImposteriser;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Test
+public class InMemoryReferenceRepositoryTest {
+    private Mockery mockery;
+
+    @BeforeMethod
+    protected void beforeEach()
+    {
+        mockery = new Mockery()
+        {
+            {
+                setImposteriser(ClassImposteriser.INSTANCE);
+            }
+        };
+    }
+
+    @AfterMethod
+    protected void afterEach()
+    {
+        mockery.assertIsSatisfied();
+    }
+
+
+}
