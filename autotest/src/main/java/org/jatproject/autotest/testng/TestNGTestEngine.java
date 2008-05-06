842053f58a93af52f4ecd5c70079cdb3ecf6ed83
Adam Esterline <aesterline@aesterline.f4tech.com>
2008-05-06 08:25:20 -0600

    Refactored TestNGTestEngine.
    We now new up a TestNG instance for every test run.   This also had the unfortunate side effect
    of forcing us to remove the TestNGTestEngineTest.

--- autotest/src/main/java/org/jatproject/autotest/testng/TestNGTestEngine.java
+++ autotest/src/main/java/org/jatproject/autotest/testng/TestNGTestEngine.java
@@ -1,37 +1,42 @@
 package org.jatproject.autotest.testng;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.jatproject.autotest.TestEngine;
 import org.jatproject.autotest.TestListener;
+import org.testng.ITestListener;
 import org.testng.TestNG;
 
 public class TestNGTestEngine implements TestEngine
 {
-    private final TestNG testng;
     private final TestNGTestAsserter asserter;
+    private List<ITestListener> listeners = new ArrayList<ITestListener>();
 
     public TestNGTestEngine()
     {
-        this(new TestNG(), new TestNGTestAsserter());
+        this(new TestNGTestAsserter());
     }
 
-    public TestNGTestEngine(TestNG testng, TestNGTestAsserter asserter)
+    public TestNGTestEngine(TestNGTestAsserter asserter)
     {
-        this.testng = testng;
         this.asserter = asserter;
-
-        testng.setVerbose(0);
-        testng.setUseDefaultListeners(false);
     }
 
     public void addListener(TestListener listener)
     {
-        testng.addListener(new TestNGTestListener(listener));
+        listeners.add(new TestNGTestListener(listener));
     }
 
     public boolean run(Class<?> clazz)
     {
         if(asserter.isTest(clazz))
         {
+            TestNG testng = new TestNG();
+            testng.setVerbose(0);
+            testng.setUseDefaultListeners(false);
+
+            for(ITestListener listener : listeners) testng.addListener(listener);
+
             testng.setTestClasses(new Class[] {clazz});
             testng.run();
             return true;
